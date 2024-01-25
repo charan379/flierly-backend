346b32c871c140a561a5462ffebfe5ff474f8775
charan379 <37008444+charan379@users.noreply.github.com>
2024-01-25 19:46:16 +0530

    added Organization entity

---
+++ src/main/java/com/ctytech/flierly/organization/entity/Organization.java
@@ -0,0 +1,18 @@
+package com.ctytech.flierly.organization.entity;
+
+import java.io.Serializable;
+
+public class Organization implements Serializable {
+
+    private String OrgId;
+
+    private String OrgName;
+
+    private String Address;
+
+    private String Contact;
+
+    private String Email;
+
+    private Long Phone;
+}
