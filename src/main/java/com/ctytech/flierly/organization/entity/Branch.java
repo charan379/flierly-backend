81cc40445f0865e1f6544e7282a71a676abf2b7e
charan379 <37008444+charan379@users.noreply.github.com>
2024-01-28 11:55:50 +0530

    moved class from branch to Organization package

--- src/main/java/com/ctytech/flierly/organization/entity/Branch.java
+++ src/main/java/com/ctytech/flierly/organization/entity/Branch.java
@@ -1,7 +1,6 @@
-package com.ctytech.flierly.branch.entity;
+package com.ctytech.flierly.organization.entity;
 
 import com.ctytech.flierly.address.entity.Address;
-import com.ctytech.flierly.organization.entity.Organization;
 import com.ctytech.flierly.taxation.entity.TaxIdentity;
 import jakarta.persistence.*;
 import jakarta.validation.constraints.Email;
