3073fba67642d1240c33a11e19e4a16817f3af56
charan379 <37008444+charan379@users.noreply.github.com>
2024-01-28 11:56:47 +0530

    added Contact entity

---
+++ src/main/java/com/ctytech/flierly/contacts/enitity/Contact.java
@@ -0,0 +1,25 @@
+package com.ctytech.flierly.contacts.enitity;
+
+import com.ctytech.flierly.organization.entity.Branch;
+import jakarta.persistence.*;
+import jakarta.validation.constraints.NotNull;
+import lombok.EqualsAndHashCode;
+import lombok.Getter;
+import lombok.Setter;
+
+@Entity
+@Table(name = "contacts")
+@Getter @Setter @EqualsAndHashCode
+public class Contact {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @ManyToOne(cascade = CascadeType.DETACH, fetch = FetchType.EAGER)
+    @JoinColumn(name = "branchId", referencedColumnName = "id", foreignKey = @ForeignKey(name = "contact_and_branch_fkey"), updatable = false)
+    private Branch branch;
+
+    @NotNull
+    private String name;
+}
