c7a939cebed8fd9fd914f1d66e633c7fa6519419
charan379 <37008444+charan379@users.noreply.github.com>
2024-01-28 11:49:14 +0530

    added AccountType entity

---
+++ src/main/java/com/ctytech/flierly/account/entity/AccountType.java
@@ -0,0 +1,33 @@
+package com.ctytech.flierly.account.entity;
+
+import jakarta.persistence.*;
+import jakarta.validation.constraints.NotNull;
+import lombok.EqualsAndHashCode;
+import lombok.Getter;
+import lombok.Setter;
+
+import java.io.Serializable;
+import java.util.HashSet;
+import java.util.Set;
+
+@Entity
+@Table(name = "accountTypes")
+@Getter @Setter @EqualsAndHashCode
+public class AccountType implements Serializable {
+
+    @Id
+    @Column(nullable = false, updatable = false, length = 60)
+    private String id;
+
+    @NotNull
+    private String name;
+
+    @NotNull
+    @ManyToMany(cascade = CascadeType.DETACH, fetch = FetchType.EAGER)
+    @JoinTable(name = "accountTypeSubtypeAssociations",joinColumns = {
+            @JoinColumn(name = "accountType", referencedColumnName = "id")
+    },inverseJoinColumns =  {
+            @JoinColumn(name = "accountSubtype", referencedColumnName = "id")
+    })
+    private Set<AccountSubtype> accountSubtypes = new HashSet<AccountSubtype>();
+}
