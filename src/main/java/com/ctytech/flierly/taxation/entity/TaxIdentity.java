20ce891699293fba65a7ead7c62620b61c087fcb
charan379 <37008444+charan379@users.noreply.github.com>
2024-01-28 12:01:22 +0530

    replaced OrgId association with branchId

--- src/main/java/com/ctytech/flierly/taxation/entity/TaxIdentity.java
+++ src/main/java/com/ctytech/flierly/taxation/entity/TaxIdentity.java
@@ -1,6 +1,6 @@
 package com.ctytech.flierly.taxation.entity;
 
-import com.ctytech.flierly.organization.entity.Organization;
+import com.ctytech.flierly.organization.entity.Branch;
 import jakarta.persistence.*;
 import jakarta.validation.constraints.NotNull;
 import lombok.EqualsAndHashCode;
@@ -13,10 +13,12 @@ import java.time.LocalDate;
 @Entity
 @Getter @Setter @EqualsAndHashCode
 @Table(name = "taxIdentities", uniqueConstraints = {
-        // orgId + gst
-        @UniqueConstraint(name = "uniqueOrgIdAndGst", columnNames = {"orgId", "gst"}),
-        // orgId + vat
-        @UniqueConstraint(name = "uniqueOrgIdAndVat", columnNames = {"orgId", "vat" })
+        // branchId + gst
+        @UniqueConstraint(columnNames = {"branchId", "gst"}, name = "branch_and_gst_ukey"),
+        // branchId + vat
+        @UniqueConstraint(columnNames = {"branchId", "vat" }, name = "branch_and_vat_ukey"),
+        // branchId + pan
+        @UniqueConstraint(columnNames = {"branchId", "pan" }, name = "branch_and_pan_ukey")
 })
 public class TaxIdentity implements Serializable {
 
@@ -25,8 +27,8 @@ public class TaxIdentity implements Serializable {
     private Long id;
 
     @ManyToOne(cascade = CascadeType.DETACH, fetch = FetchType.EAGER)
-    @JoinColumn(name = "orgId" ,referencedColumnName = "id", foreignKey = @ForeignKey(name = "tax_identity_org_fkey"))
-    private Organization organization;
+    @JoinColumn(name = "branchId", referencedColumnName = "id", foreignKey = @ForeignKey(name = "tax_identity_and_branch_fkey"), updatable = false)
+    private Branch branch;
 
     @NotNull
     private String country;
@@ -46,6 +48,4 @@ public class TaxIdentity implements Serializable {
     private String pan;
 
     private String vat;
-
-
 }
