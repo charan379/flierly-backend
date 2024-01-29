6780cabbe55239c388a9dfa32645aaacef6dd012
charan379 <37008444+charan379@users.noreply.github.com>
2024-01-29 18:44:48 +0530

    added district entity

---
+++ src/main/java/com/ctytech/flierly/address/entity/District.java
@@ -0,0 +1,39 @@
+package com.ctytech.flierly.address.entity;
+
+import jakarta.persistence.*;
+import jakarta.validation.constraints.NotNull;
+import lombok.EqualsAndHashCode;
+import lombok.Getter;
+import lombok.Setter;
+
+import java.util.HashSet;
+import java.util.Set;
+
+@Entity
+@Table(name = "districts", uniqueConstraints = {
+        @UniqueConstraint(columnNames = {"nameCode", "stateId"})
+})
+@Getter @Setter @EqualsAndHashCode
+public class District {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Integer id;
+
+    @NotNull
+    @Column(updatable = false, length = 100)
+    private String nameCode;
+
+    @NotNull
+    private String name;
+
+    private Integer landlineCode;
+
+    @ManyToOne(cascade = CascadeType.DETACH, fetch = FetchType.EAGER)
+    @JoinColumn(name = "stateId", referencedColumnName = "id", foreignKey = @ForeignKey(name = "district_and_state_fkey"))
+    private State state;
+
+    @OneToMany(cascade = CascadeType.DETACH, fetch = FetchType.LAZY, mappedBy = "district")
+    private Set<City> cities = new HashSet<City>();
+
+}
