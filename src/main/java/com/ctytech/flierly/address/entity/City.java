8bb6f3cc1e3d2a88db72bf3b591b3411a4ff5dbc
charan379 <37008444+charan379@users.noreply.github.com>
2024-01-29 18:44:18 +0530

    added city entity

---
+++ src/main/java/com/ctytech/flierly/address/entity/City.java
@@ -0,0 +1,26 @@
+package com.ctytech.flierly.address.entity;
+
+import jakarta.persistence.*;
+import jakarta.validation.constraints.NotNull;
+
+import java.util.HashSet;
+import java.util.Set;
+
+@Entity
+@Table(name = "cities")
+public class City {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @NotNull
+    private String name;
+
+    @ManyToOne(cascade = CascadeType.DETACH, fetch = FetchType.EAGER)
+    @JoinColumn(name = "districtId", referencedColumnName = "id", foreignKey = @ForeignKey(name = "city_district_fkey"))
+    private District district;
+
+    @OneToMany(cascade = CascadeType.DETACH, fetch = FetchType.LAZY, mappedBy = "city")
+    private Set<PostalIdentity> postalIdentities = new HashSet<PostalIdentity>();
+
+}
