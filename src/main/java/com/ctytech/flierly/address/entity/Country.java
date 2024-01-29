c8202b3c3575b74b855cb0c09f7cc6e2c18c00fc
charan379 <37008444+charan379@users.noreply.github.com>
2024-01-29 18:44:31 +0530

    added country entity

---
+++ src/main/java/com/ctytech/flierly/address/entity/Country.java
@@ -0,0 +1,34 @@
+package com.ctytech.flierly.address.entity;
+
+import jakarta.persistence.*;
+import jakarta.validation.constraints.NotNull;
+import lombok.EqualsAndHashCode;
+import lombok.Getter;
+import lombok.Setter;
+
+import java.util.HashSet;
+import java.util.Set;
+
+@Entity
+@Table(name = "countries")
+@Getter @Setter @EqualsAndHashCode
+public class Country {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Integer id;
+
+    @NotNull
+    @Column(length = 100, unique =true, updatable = false)
+    private String nameCode;
+
+    @NotNull
+    private String name;
+
+    @NotNull
+    @Column(unique = true)
+    private Integer dialingCode;
+
+    @OneToMany(cascade = CascadeType.DETACH, fetch = FetchType.LAZY, mappedBy = "country")
+    private Set<State> states = new HashSet<State>();
+}
