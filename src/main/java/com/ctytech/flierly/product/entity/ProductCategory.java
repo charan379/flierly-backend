157c94642064addec682204046abef8d5247d583
charan379 <37008444+charan379@users.noreply.github.com>
2024-02-03 22:27:18 +0530

    added ProductCategory entity

---
+++ src/main/java/com/ctytech/flierly/product/entity/ProductCategory.java
@@ -0,0 +1,30 @@
+package com.ctytech.flierly.product.entity;
+
+import jakarta.persistence.*;
+import jakarta.validation.constraints.NotNull;
+import lombok.EqualsAndHashCode;
+import lombok.Getter;
+import lombok.Setter;
+
+import java.util.HashSet;
+import java.util.Set;
+
+@Entity
+@Table(name = "ProductCategories")
+@Getter @Setter @EqualsAndHashCode
+public class ProductCategory {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Integer id;
+
+    @NotNull
+    @Column(updatable = false, length = 65, unique = true)
+    private String nameCode;
+
+    @NotNull
+    private String displayName;
+
+    @OneToMany(cascade = CascadeType.DETACH, fetch = FetchType.LAZY, mappedBy = "productCategory")
+    private Set<ProductGroup> productGroups = new HashSet<>();
+}
