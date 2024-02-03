1bb5abf61785c8dd971d0dc5496f650935b55515
charan379 <37008444+charan379@users.noreply.github.com>
2024-02-03 22:29:50 +0530

    added ProductAttributes, ProductAttributeSchemas, name, isSerialized properties

--- src/main/java/com/ctytech/flierly/product/entity/Product.java
+++ src/main/java/com/ctytech/flierly/product/entity/Product.java
@@ -2,9 +2,13 @@ package com.ctytech.flierly.product.entity;
 
 import com.ctytech.flierly.organization.entity.Branch;
 import jakarta.persistence.*;
+import jakarta.validation.constraints.NotNull;
 import lombok.EqualsAndHashCode;
 import lombok.Getter;
 import lombok.Setter;
+
+import java.util.HashSet;
+import java.util.Set;
 
 @Entity
 @Table(name = "products")
@@ -18,4 +22,21 @@ public class Product {
     @ManyToOne(cascade = CascadeType.DETACH, fetch = FetchType.EAGER)
     @JoinColumn(name = "branchId", referencedColumnName = "id", foreignKey = @ForeignKey(name = "product_branch_fkey"))
     private Branch branch;
+
+    @NotNull
+    @Column(unique = true)
+    private String name;
+
+    private Boolean isSerialized;
+
+    @OneToMany(cascade = CascadeType.ALL, fetch = FetchType.LAZY, mappedBy = "product")
+    private Set<ProductAttribute> productAttributes = new HashSet<>();
+
+    @ManyToMany(cascade = CascadeType.DETACH, fetch = FetchType.LAZY)
+    @JoinTable(name = "productAttributeSchemaMappings", joinColumns = {
+            @JoinColumn(name = "productId", referencedColumnName = "id")
+    }, inverseJoinColumns = {
+            @JoinColumn(name = "attributeSchemaId", referencedColumnName = "id")
+    })
+    private Set<ProductAttributeSchema> productAttributeSchemas = new HashSet<>();
 }
