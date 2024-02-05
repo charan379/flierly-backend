9f280b5222e8f51ee41bdf2eafa1019c094c0c66
charan379 <37008444+charan379@users.noreply.github.com>
2024-02-05 18:33:43 +0530

    simple test class

--- src/test/java/com/ctytech/flierly/FlierlyApplicationTests.java
+++ src/test/java/com/ctytech/flierly/FlierlyApplicationTests.java
@@ -1,7 +1,12 @@
 package com.ctytech.flierly;
 
+import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
+
+import java.math.BigDecimal;
+import java.math.MathContext;
+import java.math.RoundingMode;
 
 @SpringBootTest
 class FlierlyApplicationTests {
@@ -9,5 +14,23 @@ class FlierlyApplicationTests {
 	@Test
 	void contextLoads() {
 	}
+
+	@Test
+	@DisplayName("Simple Test : UOM")
+	void simpleTest() {
+		BigDecimal qty = BigDecimal.valueOf(105);
+
+		String fromUom = "Pack Of 3";
+
+		String toUom = "Each";
+
+		BigDecimal conversionFactor = BigDecimal.valueOf(0.3333333330);
+
+		BigDecimal qtyCon = qty.multiply(conversionFactor, new MathContext(10, RoundingMode.HALF_EVEN));
+
+		System.out.printf("This converted qty to packs is : %f", qtyCon.stripTrailingZeros());
+
+	}
+
 
 }
