d2ca0a405ecc1eab500cca9c65d3805fa4cc6736
charan379 <37008444+charan379@users.noreply.github.com>
2024-02-05 18:41:30 +0530

    added UomDTO

---
+++ src/main/java/com/ctytech/flierly/uom/dto/UomDTO.java
@@ -0,0 +1,15 @@
+package com.ctytech.flierly.uom.dto;
+
+import lombok.EqualsAndHashCode;
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter @Setter @EqualsAndHashCode
+public class UomDTO {
+
+    private Long id;
+
+    private String code;
+
+    private String name;
+}
