0e9455e887cc259005a65501e035bbd2aeab879d
charan379 <37008444+charan379@users.noreply.github.com>
2024-02-06 21:22:11 +0530

    Added AssetDTO

---
+++ src/main/java/com/ctytech/flierly/asset/dto/AssetDTO.java
@@ -0,0 +1,12 @@
+package com.ctytech.flierly.asset.dto;
+
+import lombok.EqualsAndHashCode;
+import lombok.Getter;
+import lombok.Setter;
+
+import java.io.Serializable;
+
+@Getter @Setter @EqualsAndHashCode
+public class AssetDTO implements Serializable {
+
+}
