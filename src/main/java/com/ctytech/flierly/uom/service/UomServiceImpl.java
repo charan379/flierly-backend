62398db1ff8b36c20e91600d8a1b28f7edeafe19
charan379 <37008444+charan379@users.noreply.github.com>
2024-02-06 14:24:13 +0530

    minor naming convention changes and tweaks

--- src/main/java/com/ctytech/flierly/uom/service/UomServiceImpl.java
+++ src/main/java/com/ctytech/flierly/uom/service/UomServiceImpl.java
@@ -1,46 +1,53 @@
 package com.ctytech.flierly.uom.service;
 
 import com.ctytech.flierly.uom.dto.UomDTO;
+import com.ctytech.flierly.uom.dto.UomPageRequestDTO;
 import com.ctytech.flierly.uom.exception.UomServiceException;
-import jakarta.transaction.Transactional;
+import com.ctytech.flierly.uom.repository.UomRepository;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.domain.Page;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 
 @Service(value = "uomService")
 @Transactional
-public class UomServiceImpl implements UomService{
+public class UomServiceImpl implements UomService {
+
+    @Autowired
+    private UomRepository uomRepository;
+
     @Override
     public UomDTO save(UomDTO uomDTO) throws UomServiceException {
         return null;
     }
 
     @Override
-    public UomDTO fetchById(Long id) throws UomServiceException {
+    public UomDTO fetch(Long id) throws UomServiceException {
         return null;
     }
 
     @Override
-    public UomDTO fetchByCode(String code) throws UomServiceException {
+    public UomDTO fetch(String code) throws UomServiceException {
         return null;
     }
 
     @Override
-    public UomDTO modifyById(Long id, UomDTO update) throws UomServiceException {
+    public Page<UomDTO> fetchPage(UomPageRequestDTO uomPageRequestDTO) throws UomServiceException {
         return null;
     }
 
     @Override
-    public void removeById(Long id) throws UomServiceException {
-
-    }
-
-    @Override
-    public Boolean existsByCode(String code) throws UomServiceException {
+    public UomDTO modify(Long id, UomDTO update) throws UomServiceException {
         return null;
     }
 
     @Override
-    public Page<UomDTO> fetchByQuery(String codeQ, String nameQ, Integer pageNo, Integer resultsPerPage, String sort) throws UomServiceException {
+    public void remove(Long id) throws UomServiceException {
+
+    }
+
+    @Override
+    public Boolean codeExists(String code) throws UomServiceException {
         return null;
     }
 }
