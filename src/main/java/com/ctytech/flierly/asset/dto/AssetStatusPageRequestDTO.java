94f3b27718006797497f6d1389cfc14088e3f55b
charan379 <37008444+charan379@users.noreply.github.com>
2024-02-07 15:27:24 +0530

    assetStatus page request DTO added

---
+++ src/main/java/com/ctytech/flierly/asset/dto/AssetStatusPageRequestDTO.java
@@ -0,0 +1,14 @@
+package com.ctytech.flierly.asset.dto;
+
+public class AssetStatusPageRequestDTO {
+
+    private String code;
+
+    private String name;
+
+    private Integer pageNo;
+
+    private Integer resultsPerPage;
+
+    private String sort;
+}
