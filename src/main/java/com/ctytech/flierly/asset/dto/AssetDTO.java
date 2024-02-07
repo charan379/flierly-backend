1f53553342d71a0d1a21f2da28f25f88ecbaddb2
charan379 <37008444+charan379@users.noreply.github.com>
2024-02-07 15:23:24 +0530

    AssetDTO updated with available fields

--- src/main/java/com/ctytech/flierly/asset/dto/AssetDTO.java
+++ src/main/java/com/ctytech/flierly/asset/dto/AssetDTO.java
@@ -5,8 +5,33 @@ import lombok.Getter;
 import lombok.Setter;
 
 import java.io.Serializable;
+import java.time.LocalDate;
 
 @Getter @Setter @EqualsAndHashCode
 public class AssetDTO implements Serializable {
+
+    private Long id;
+
+//    BranchDTO
+
+    private String serialNumber;
+
+    private String imei1;
+
+    private String imei2;
+
+    private LocalDate manufacturingDate;
+
+//    Purchase Invoice DTO
+
+//    Sales Invoice DTO
+
+//    Sold To Account DTO
+
+//    Purchased From Account DTO
+
+//    Product DTO
+
+    private AssetStatusDTO assetStatus;
 
 }
