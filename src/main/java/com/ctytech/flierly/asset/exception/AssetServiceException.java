a6f970c99cf6799ffd19cd720843506a588d1fa8
charan379 <37008444+charan379@users.noreply.github.com>
2024-02-07 15:28:09 +0530

    assetService exception added

---
+++ src/main/java/com/ctytech/flierly/asset/exception/AssetServiceException.java
@@ -0,0 +1,10 @@
+package com.ctytech.flierly.asset.exception;
+
+import com.ctytech.flierly.FlierlyException;
+
+public class AssetServiceException extends FlierlyException {
+
+    public AssetServiceException(String message){
+        super(message);
+    }
+}
