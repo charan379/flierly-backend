54830b4312658d98b51e60dbbd96eee5f8ac709e
charan379 <37008444+charan379@users.noreply.github.com>
2024-02-08 22:35:47 +0530

    added id_generator, new isDeleted field, validations

--- src/main/java/com/ctytech/flierly/organization/entity/Branch.java
+++ src/main/java/com/ctytech/flierly/organization/entity/Branch.java
@@ -3,7 +3,9 @@ package com.ctytech.flierly.organization.entity;
 import com.ctytech.flierly.address.entity.Address;
 import com.ctytech.flierly.taxation.entity.TaxIdentity;
 import jakarta.persistence.*;
+import jakarta.validation.constraints.Digits;
 import jakarta.validation.constraints.Email;
+import jakarta.validation.constraints.NotBlank;
 import jakarta.validation.constraints.NotNull;
 import lombok.EqualsAndHashCode;
 import lombok.Getter;
@@ -13,34 +15,44 @@ import java.io.Serializable;
 
 @Entity
 @Table(name = "branches")
-@Getter @Setter @EqualsAndHashCode
+@SequenceGenerator(name = "branch_id_generator", sequenceName = "branch_id_seq", initialValue = 1000, allocationSize = 1)
+@Getter
+@Setter
+@EqualsAndHashCode
 public class Branch implements Serializable {
 
     @Id
-    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @GeneratedValue(strategy = GenerationType.SEQUENCE, generator = "branch_id_generator")
     private Long id;
 
-    @NotNull
+    @NotBlank(message = "{branch.name.absent}")
     private String name;
 
-    @NotNull
+    @Column(columnDefinition = "boolean default false")
+    private Boolean isDeleted;
+
+    @NotNull(message = "{branch.address.absent")
     @OneToOne(cascade = CascadeType.DETACH, fetch = FetchType.LAZY)
     @JoinColumn(name = "addressId", referencedColumnName = "id", foreignKey = @ForeignKey(name = "branch_address_fkey"), updatable = false)
     private Address address;
 
-    @NotNull
+    @NotNull(message = "{branch.taxId.absent}")
     @ManyToOne(cascade = CascadeType.DETACH, fetch = FetchType.LAZY)
     @JoinColumn(name = "taxIdentityId", referencedColumnName = "id", foreignKey = @ForeignKey(name = "branch_tax_identity_fkey"))
     private TaxIdentity taxIdentity;
 
-    @NotNull
-    private Long phone;
+    @Digits(integer = 13, fraction = 0, message = "{phone.invalid}")
+    @NotBlank(message = "{branch.phone.absent}")
+    @Column(unique = true, nullable = false)
+    private String phone;
 
-    @NotNull
-    private Long alternatePhone;
+    @Digits(integer = 13, fraction = 0, message = "{phone.invalid}")
+    @Column(unique = true)
+    private String alternatePhone;
 
-    @NotNull
-    @Email
+    @Email(message = "{email.invalid}")
+    @NotBlank(message = "{branch.email.absent}")
+    @Column(unique = true, nullable = false)
     private String email;
 
 }
