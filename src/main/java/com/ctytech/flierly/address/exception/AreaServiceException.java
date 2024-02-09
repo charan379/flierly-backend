c388c2638e769609cd1d7b328767abf086c93cd5
charan379 <37008444+charan379@users.noreply.github.com>
2024-02-09 21:33:56 +0530

    removed AreaServiceException

--- src/main/java/com/ctytech/flierly/address/exception/AreaServiceException.java
+++
@@ -1,7 +0,0 @@
-package com.ctytech.flierly.address.exception;
-
-public class AreaServiceException extends AddressServiceException {
-    public AreaServiceException(String message) {
-        super(message);
-    }
-}
