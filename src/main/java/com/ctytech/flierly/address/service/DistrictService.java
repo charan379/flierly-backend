91d043db2c7f76f1d612c384931dff63de33ef18
charan379 <37008444+charan379@users.noreply.github.com>
2024-02-10 22:10:41 +0530

    Added DistrictService

---
+++ src/main/java/com/ctytech/flierly/address/service/DistrictService.java
@@ -0,0 +1,19 @@
+package com.ctytech.flierly.address.service;
+
+import com.ctytech.flierly.address.dto.DistrictDTO;
+import com.ctytech.flierly.address.exception.DistrictServiceException;
+
+import java.util.List;
+
+public interface DistrictService {
+
+    DistrictDTO save(DistrictDTO districtDTO) throws DistrictServiceException;
+
+    DistrictDTO fetch(Long id) throws DistrictServiceException;
+
+    List<DistrictDTO> fetchAllByStateId(Long stateId) throws DistrictServiceException;
+
+    DistrictDTO modify(Long id, DistrictDTO update) throws DistrictServiceException;
+
+    void remove(Long id) throws DistrictServiceException;
+}
