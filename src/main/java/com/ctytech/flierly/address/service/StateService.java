a3c6d67c9a7626305efdcb7efbf25d42fcc5fcf8
charan379 <37008444+charan379@users.noreply.github.com>
2024-02-10 22:10:55 +0530

    Added StateService

---
+++ src/main/java/com/ctytech/flierly/address/service/StateService.java
@@ -0,0 +1,19 @@
+package com.ctytech.flierly.address.service;
+
+import com.ctytech.flierly.address.dto.StateDTO;
+import com.ctytech.flierly.address.exception.StateServiceException;
+
+import java.util.List;
+
+public interface StateService {
+
+    StateDTO save(StateDTO stateDTO) throws StateServiceException;
+
+    StateDTO fetch(Long id) throws StateServiceException;
+
+    List<StateDTO> fetchByCountryId(Long id) throws StateServiceException;
+
+    StateDTO modify(Long id, StateDTO update) throws StateServiceException;
+
+    void remove(Long id) throws StateServiceException;
+}
