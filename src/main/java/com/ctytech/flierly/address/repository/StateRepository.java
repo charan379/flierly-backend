7c513bad01d001187a38dd8cb7af1ed5a212e74c
charan379 <37008444+charan379@users.noreply.github.com>
2024-02-11 20:31:39 +0530

    added existsByGstCodeAndCountryId, existsByCodeAndCountryId

--- src/main/java/com/ctytech/flierly/address/repository/StateRepository.java
+++ src/main/java/com/ctytech/flierly/address/repository/StateRepository.java
@@ -2,8 +2,16 @@ package com.ctytech.flierly.address.repository;
 
 import com.ctytech.flierly.address.entity.State;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
 
 @Repository
 public interface StateRepository extends JpaRepository<State, Long> {
+
+    @Query("SELECT CASE WHEN count(S) > 0 THEN true ELSE false END FROM State S WHERE S.gstCode= :gstCode AND S.country.id= :countryId")
+    Boolean existsByGstCodeAndCountryId(@Param("gstCode") Integer gstCode, @Param("countryId") Long countryId);
+
+    @Query("SELECT CASE WHEN count(S) > 0 THEN true ELSE false END FROM State S WHERE S.code= :code AND S.country.id= :countryId")
+    Boolean existsByCodeAndCountryId(@Param("code") String code, @Param("countryId") Long countryId);
 }
