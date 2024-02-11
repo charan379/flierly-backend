7154c130bf4f6f1968f1ca8ed455454aadc0a27e
charan379 <37008444+charan379@users.noreply.github.com>
2024-02-11 14:32:03 +0530

    new methods added

--- src/main/java/com/ctytech/flierly/address/repository/CountryRepository.java
+++ src/main/java/com/ctytech/flierly/address/repository/CountryRepository.java
@@ -2,9 +2,20 @@ package com.ctytech.flierly.address.repository;
 
 import com.ctytech.flierly.address.entity.Country;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
+
+import java.util.Optional;
 
 @Repository
 public interface CountryRepository extends JpaRepository<Country, Long> {
-    
+
+    Optional<Country> findByCode(String code);
+
+    @Query("SELECT CASE WHEN count(C) > 0 THEN true ELSE false END FROM Country C WHERE C.code= :code")
+    Boolean existsByCode(@Param("code") String code);
+
+    @Query("SELECT CASE WHEN count(C) > 0 THEN true ELSE false END from Country C WHERE C.dialingCode= :dialingCode")
+    Boolean existsByDialingCode(@Param("dialingCode") Integer dialingCode);
 }
