f2c6ae3c9d5205e72d7e1d474f4fd9d8c6257c77
charan379 <37008444+charan379@users.noreply.github.com>
2024-02-13 13:54:48 +0530

    added validation to code

--- src/main/java/com/ctytech/flierly/address/dto/CityDTO.java
+++ src/main/java/com/ctytech/flierly/address/dto/CityDTO.java
@@ -1,6 +1,8 @@
 package com.ctytech.flierly.address.dto;
 
 import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.Pattern;
+import jakarta.validation.constraints.Size;
 import lombok.EqualsAndHashCode;
 import lombok.Getter;
 import lombok.Setter;
@@ -13,6 +15,11 @@ import java.io.Serializable;
 public class CityDTO implements Serializable {
 
     private Long id;
+
+    @NotBlank(message = "{city.code.absent}")
+    @Size(max = 100, message = "{code.size.invalid}")
+    @Pattern(regexp = "^[a-z_]+$", message = "{code.pattern.invalid}")
+    private String code;
 
     @NotBlank(message = "{city.name.absent}")
     private String name;
--- src/main/java/com/ctytech/flierly/address/entity/City.java
+++ src/main/java/com/ctytech/flierly/address/entity/City.java
@@ -2,6 +2,8 @@ package com.ctytech.flierly.address.entity;
 
 import jakarta.persistence.*;
 import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.Pattern;
+import jakarta.validation.constraints.Size;
 import lombok.EqualsAndHashCode;
 import lombok.Getter;
 import lombok.Setter;
@@ -17,6 +19,12 @@ public class City {
     @Id
     @GeneratedValue(strategy = GenerationType.SEQUENCE, generator = "city_id_generator")
     private Long id;
+
+    @NotBlank(message = "{city.code.absent}")
+    @Size(max = 100, message = "{code.size.invalid}")
+    @Pattern(regexp = "^[a-z_]+$", message = "{code.pattern.invalid}")
+    @Column(updatable = false, length = 100)
+    private String code;
 
     @NotBlank(message = "{city.name.absent}")
     private String name;
