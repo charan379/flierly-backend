65361add71ef3b7f1d0d1fd24d082e2f7ada0027
charan379 <37008444+charan379@users.noreply.github.com>
2024-02-13 16:58:55 +0530

    added repository methods to fetch PI

--- src/main/java/com/ctytech/flierly/address/repository/PostalIdentityRepository.java
+++ src/main/java/com/ctytech/flierly/address/repository/PostalIdentityRepository.java
@@ -6,9 +6,22 @@ import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
 
+import java.util.List;
+import java.util.Optional;
+
 @Repository
 public interface PostalIdentityRepository extends JpaRepository<PostalIdentity, Long> {
 
-    @Query("SELECT CASE WHEN count(PI) > 0 THEN true ELSE false END FROM PostalIdentity PI WHERE PI.city.id = :cityId AND PI.pinCode= :pinCode")
-    boolean existsByCityIdAndPincode(@Param("cityId") Long cityId, @Param("pinCode") Integer pincode);
+
+    @Query("SELECT PI FROM PostalIdentity PI WHERE PI.country.id= :countryId AND PI.pinCode= :pincode")
+    Optional<PostalIdentity> fetchByCountryIdAndPincode(@Param("countryId") Long countryId, @Param("pincode") Integer pincode);
+
+    @Query("SELECT PI FROM PostalIdentity PI WHERE PI.city.id= :cityId")
+    List<PostalIdentity> fetchAllByCityId(@Param("cityId") Long cityId);
+
+    @Query("SELECT CASE WHEN count(PI) > 0 THEN true ELSE false END FROM PostalIdentity PI WHERE PI.country.id= :countryId AND PI.pinCode= :pincode")
+    Boolean exitsByCountryIdAndPincode(@Param("countryId") Long countryId, @Param("pincode") Integer pincode);
+
+    @Query("SELECT CASE WHEN count(C) > 0 THEN true ELSE false END FROM City C WHERE C.id= :cityId AND C.district.state.country.id= :countryId")
+    Boolean isCityBelongsToCountry(@Param("cityId") Long cityId, @Param("countryId") Long countryId);
 }
