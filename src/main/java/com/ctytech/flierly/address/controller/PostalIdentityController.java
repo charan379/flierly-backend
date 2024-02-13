6bb57fc2e47270a41b7f4f889a8fb71eb065c8cd
charan379 <37008444+charan379@users.noreply.github.com>
2024-02-13 22:34:23 +0530

    added controller end points for PI

--- src/main/java/com/ctytech/flierly/address/controller/PostalIdentityController.java
+++ src/main/java/com/ctytech/flierly/address/controller/PostalIdentityController.java
@@ -10,6 +10,8 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.validation.annotation.Validated;
 import org.springframework.web.bind.annotation.*;
 
+import java.util.List;
+
 @RestController
 @RequestMapping(value = "/postal-id")
 @Validated
@@ -20,23 +22,43 @@ public class PostalIdentityController {
 
     @PostMapping
     public ResponseEntity<PostalIdentityDTO> createPI(@RequestBody @Valid PostalIdentityDTO newPostalIdentityDTO) throws PostalIdentityServiceException {
-
         PostalIdentityDTO postalIdentityDTO = postalIdentityService.save(newPostalIdentityDTO);
-
         return new ResponseEntity<>(postalIdentityDTO, HttpStatus.CREATED);
     }
 
     @GetMapping(value = "/{id}")
     public ResponseEntity<PostalIdentityDTO> getPIById(@PathVariable(name = "id") Long id) throws PostalIdentityServiceException {
         PostalIdentityDTO postalIdentityDTO = postalIdentityService.fetch(id);
-
         return new ResponseEntity<>(postalIdentityDTO, HttpStatus.OK);
     }
 
-    @PutMapping(value = "/{id}")
-    public ResponseEntity<PostalIdentityDTO> updatePIById(@PathVariable(name = "id") Long id, @RequestBody @Valid PostalIdentityDTO update) throws PostalIdentityServiceException {
-        PostalIdentityDTO postalIdentityDTO = postalIdentityService.modify(id, update);
+    @GetMapping(value = "/pincode/{pincode}}")
+    public ResponseEntity<PostalIdentityDTO> getByCountryIdAndPincode(@PathVariable(name = "pincode") Integer pincode, @RequestParam(name = "countryId") Long countryId) throws PostalIdentityServiceException {
+        PostalIdentityDTO postalIdentityDTO = postalIdentityService.fetchByCountryIdAndPincode(countryId, pincode);
+        return new ResponseEntity<>(postalIdentityDTO, HttpStatus.OK);
+    }
 
+    @GetMapping
+    public ResponseEntity<List<PostalIdentityDTO>> getAllByCityId(@RequestParam(name = "cityId") Long cityId) {
+        List<PostalIdentityDTO> listOfPIs = postalIdentityService.fetchAllByCityId(cityId);
+        return new ResponseEntity<>(listOfPIs, HttpStatus.OK);
+    }
+
+    @GetMapping(value = "/pincode-exists/{pincode}")
+    public ResponseEntity<Boolean> checkExistenceWithCountryIdAndPincode(@PathVariable(name = "pincode") Integer pincode, @RequestParam(name = "countryId") Long countryId) {
+        Boolean isExists = postalIdentityService.existsByCountryIdAndPincode(countryId, pincode);
+        return new ResponseEntity<>(isExists, HttpStatus.OK);
+    }
+
+    @GetMapping(value = "/valid-country-city")
+    public ResponseEntity<Boolean> checkIfCityBelongsToCountry(@RequestParam(name = "countryId") Long countryId, @RequestParam(name = "cityId") Long cityId) {
+        Boolean isTrue = postalIdentityService.isCityBelongsToCountry(countryId, cityId);
+        return new ResponseEntity<>(isTrue, HttpStatus.OK);
+    }
+
+    @PutMapping(value = "/{id}")
+    public ResponseEntity<PostalIdentityDTO> updateById(@PathVariable(name = "id") Long id, @RequestBody @Valid PostalIdentityDTO update) throws PostalIdentityServiceException {
+        PostalIdentityDTO postalIdentityDTO = postalIdentityService.modify(id, update);
         return new ResponseEntity<>(postalIdentityDTO, HttpStatus.OK);
     }
 }
