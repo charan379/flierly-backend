4a795e21be6ba142321c0954f305c017d91f1854
charan379 <37008444+charan379@users.noreply.github.com>
2024-02-13 17:00:07 +0530

    PI Service interface updated

--- src/main/java/com/ctytech/flierly/address/service/PostalIdentityService.java
+++ src/main/java/com/ctytech/flierly/address/service/PostalIdentityService.java
@@ -3,17 +3,23 @@ package com.ctytech.flierly.address.service;
 import com.ctytech.flierly.address.dto.PostalIdentityDTO;
 import com.ctytech.flierly.address.exception.PostalIdentityServiceException;
 
+import java.util.List;
+
 public interface PostalIdentityService {
 
     PostalIdentityDTO save(PostalIdentityDTO postalIdentityDTO) throws PostalIdentityServiceException;
 
     PostalIdentityDTO fetch(Long id) throws PostalIdentityServiceException;
 
+    PostalIdentityDTO fetchByCountryIdAndPincode(Long countryId, Integer pincode) throws PostalIdentityServiceException;
+
+    List<PostalIdentityDTO> fetchAllByCityId(Long cityId);
+
     PostalIdentityDTO modify(Long id, PostalIdentityDTO update) throws PostalIdentityServiceException;
 
     PostalIdentityDTO remove(Long id) throws PostalIdentityServiceException;
 
-    Boolean existsByCityIdAndPincode(Long cityId, Integer pincode) throws PostalIdentityServiceException;
+    Boolean existsByCountryIdAndPincode(Long countryId, Integer pincode);
 
-
+    Boolean isCityBelongsToCountry(Long cityId, Long countryId);
 }
