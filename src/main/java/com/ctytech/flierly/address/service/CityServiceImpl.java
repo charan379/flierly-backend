c68f0d4f2aa532191ba613dce416eafb08f4a8ef
charan379 <37008444+charan379@users.noreply.github.com>
2024-02-13 13:58:33 +0530

    added implementation for cityService

--- src/main/java/com/ctytech/flierly/address/service/CityServiceImpl.java
+++ src/main/java/com/ctytech/flierly/address/service/CityServiceImpl.java
@@ -1,35 +1,73 @@
 package com.ctytech.flierly.address.service;
 
 import com.ctytech.flierly.address.dto.CityDTO;
+import com.ctytech.flierly.address.dto.DistrictDTO;
+import com.ctytech.flierly.address.entity.City;
 import com.ctytech.flierly.address.exception.CityServiceException;
+import com.ctytech.flierly.address.mapper.CityMapper;
+import com.ctytech.flierly.address.repository.CityRepository;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
+import java.util.Optional;
 
 @Service(value = "cityService")
 public class CityServiceImpl implements CityService {
+
+    @Autowired
+    private CityRepository cityRepository;
+
+    @Autowired
+    private CityMapper cityMapper;
+
     @Override
     public CityDTO save(CityDTO cityDTO) throws CityServiceException {
-        return null;
+        Long districtId = Optional.ofNullable(cityDTO.getDistrict()).map(DistrictDTO::getId).orElseThrow(() -> new CityServiceException("CityService.CITY_DISTRICT_ID_ABSENT"));
+
+        if (existsByCodeAndDistrictId(cityDTO.getCode(), districtId))
+            throw new CityServiceException("CityService.CODE_ALREADY_EXISTS");
+
+        City city = cityMapper.toEntity(cityDTO);
+
+        return cityMapper.toDTO(cityRepository.save(city));
     }
 
     @Override
     public CityDTO fetch(Long id) throws CityServiceException {
-        return null;
+        City city = cityRepository.findById(id).orElseThrow(() -> new CityServiceException("CityService.NOT_FOUND"));
+        return cityMapper.toDTO(city);
     }
 
     @Override
-    public List<CityDTO> fetchAllByDistrictId(Long DistrictId) throws CityServiceException {
-        return null;
+    public CityDTO fetch(String code, Long districtId) throws CityServiceException {
+        City city = cityRepository.findByCodeAndDistrictId(code, districtId).orElseThrow(() -> new CityServiceException("CityService.NOT_FOUND"));
+        return cityMapper.toDTO(city);
+    }
+
+    @Override
+    public List<CityDTO> fetchAllByDistrictId(Long districtId) {
+        return cityRepository.findAllByDistrictId(districtId).stream().map(city -> cityMapper.toDTO(city)).toList();
     }
 
     @Override
     public CityDTO modify(Long id, CityDTO update) throws CityServiceException {
-        return null;
+        City city = cityRepository.findById(id).orElseThrow(() -> new CityServiceException("CityService.NOT_FOUND"));
+
+        if (city.getName().equals(update.getName())) return cityMapper.toDTO(city);
+
+        city.setName(update.getName());
+
+        return cityMapper.toDTO(cityRepository.save(city));
     }
 
     @Override
     public void remove(Long id) throws CityServiceException {
 
+    }
+
+    @Override
+    public Boolean existsByCodeAndDistrictId(String code, Long districtId) {
+        return cityRepository.existsByCodeAndDistrictId(code, districtId);
     }
 }
