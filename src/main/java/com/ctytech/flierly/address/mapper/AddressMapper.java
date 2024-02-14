76b4a645478ba622ba5ebf483a01be4a442eee93
charan379 <37008444+charan379@users.noreply.github.com>
2024-02-14 23:40:59 +0530

    AddressMapper updated for minimal details

--- src/main/java/com/ctytech/flierly/address/mapper/AddressMapper.java
+++ src/main/java/com/ctytech/flierly/address/mapper/AddressMapper.java
@@ -11,9 +11,37 @@ public class AddressMapper {
 
     @Autowired
     private ModelMapper modelMapper;
+    @Autowired
+    private CountryMapper countryMapper;
+    @Autowired
+    private StateMapper stateMapper;
+    @Autowired
+    private DistrictMapper districtMapper;
+    @Autowired
+    private CityMapper cityMapper;
+    @Autowired
+    private PostalIdentityMapper postalIdentityMapper;
+    @Autowired
+    private AreaMapper areaMapper;
 
     public AddressDTO toDTO(Address address) {
-        return modelMapper.map(address, AddressDTO.class);
+        AddressDTO dto = new AddressDTO();
+        dto.setId(address.getId());
+        dto.setIsActive(address.getIsActive());
+        dto.setIsPrimary(address.getIsPrimary());
+        dto.setLine1(address.getLine1());
+        dto.setLine2(address.getLine2());
+        dto.setLine3(address.getLine3());
+        dto.setCountry(countryMapper.toDTO(address.getCountry()));
+        dto.setState(stateMapper.toDTO(address.getState(), true));
+        dto.setDistrict(districtMapper.toDTO(address.getDistrict(), true));
+        dto.setCity(cityMapper.toDTO(address.getCity(), true));
+        dto.setPostalIdentity(postalIdentityMapper.toDTO(address.getPostalIdentity(), true));
+        dto.setArea(areaMapper.toDTO(address.getArea(), true));
+        dto.setLandMark(address.getLandMark());
+        dto.setLatitude(address.getLatitude());
+        dto.setLongitude(address.getLongitude());
+        return dto;
     }
 
     public Address toEntity(AddressDTO addressDTO) {
