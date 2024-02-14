52dc759c05114b1e18c5f0a30eba25f0d7fffac2
charan379 <37008444+charan379@users.noreply.github.com>
2024-02-14 12:07:23 +0530

    added repository methods to retrieve get area entities.

--- src/main/java/com/ctytech/flierly/address/repository/AreaRepository.java
+++ src/main/java/com/ctytech/flierly/address/repository/AreaRepository.java
@@ -1,7 +1,6 @@
 package com.ctytech.flierly.address.repository;
 
 import com.ctytech.flierly.address.entity.Area;
-import com.ctytech.flierly.address.exception.AreaServiceException;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.query.Param;
@@ -12,6 +11,9 @@ import java.util.List;
 @Repository
 public interface AreaRepository extends JpaRepository<Area, Long> {
 
-    @Query("SELECT A FROM Area A WHERE A.postalIdentity.id= :pId")
-    List<Area> findAllByPostalIdentityId(@Param("pId") Long pId) throws AreaServiceException;
+    @Query(value = "SELECT * FROM areas A WHERE A.postal_id= :pId", nativeQuery = true)
+    List<Area> findAllByPostalIdentityId(@Param("pId") Long pId);
+
+    @Query(value = "SELECT CASE WHEN count(A) > 0 THEN true ELSE false END FROM areas A WHERE A.postal_id= :pId AND A.name ILIKE :name", nativeQuery = true)
+    Boolean existsByPidAndName(@Param("pId") Long pId, @Param("name") String name);
 }
