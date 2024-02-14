f7a475cb2be175a4748d63ec8c420f0a1e02a9dc
charan379 <37008444+charan379@users.noreply.github.com>
2024-02-14 15:11:14 +0530

    added without pi method

--- src/main/java/com/ctytech/flierly/address/mapper/AreaMapper.java
+++ src/main/java/com/ctytech/flierly/address/mapper/AreaMapper.java
@@ -16,6 +16,13 @@ public class AreaMapper {
         return modelMapper.map(area, AreaDTO.class);
     }
 
+    public AreaDTO toDTOWithoutPI(Area area) {
+        AreaDTO areaDTO = new AreaDTO();
+        areaDTO.setId(area.getId());
+        areaDTO.setName(area.getName());
+        return areaDTO;
+    }
+
     public Area toEntity(AreaDTO areaDTO) {
         return modelMapper.map(areaDTO, Area.class);
     }
