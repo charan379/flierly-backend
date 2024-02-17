c809a1d52db35330d374fedd3ef3fb614931fe90
charan379 <37008444+charan379@users.noreply.github.com>
2024-02-17 22:53:46 +0530

    added AccountSubtypeDTO

---
+++ src/main/java/com/ctytech/flierly/account/dto/AccountSubtypeDTO.java
@@ -0,0 +1,21 @@
+package com.ctytech.flierly.account.dto;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.Pattern;
+import lombok.Data;
+
+import java.io.Serializable;
+
+@Data
+public class AccountSubtypeDTO implements Serializable {
+
+    private Long id;
+
+    @NotBlank(message = "account.subtype.name.absent")
+    @Pattern(regexp = "^[a-z0-9_]+$", message = "account.subtype.name.invalid")
+    private String name;
+}
