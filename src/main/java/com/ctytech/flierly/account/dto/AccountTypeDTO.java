be220fa9f2137ff6e0b5db9f082b2cfd53717eb9
charan379 <37008444+charan379@users.noreply.github.com>
2024-02-17 22:54:03 +0530

    added AccountTypeDTO

---
+++ src/main/java/com/ctytech/flierly/account/dto/AccountTypeDTO.java
@@ -0,0 +1,22 @@
+package com.ctytech.flierly.account.dto;
+
+import com.fasterxml.jackson.annotation.JsonInclude;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.Pattern;
+import lombok.Data;
+
+import java.io.Serializable;
+import java.util.Set;
+
+@Data
+public class AccountTypeDTO implements Serializable {
+
+    private Long id;
+
+    @NotBlank(message = "account.type.name.absent")
+    @Pattern(regexp = "^[a-z0-9_]+$", message = "account.type.name.invalid")
+    private String name;
+
+    @JsonInclude(JsonInclude.Include.NON_NULL)
+    private Set<AccountSubtypeDTO> subtypes;
+}
