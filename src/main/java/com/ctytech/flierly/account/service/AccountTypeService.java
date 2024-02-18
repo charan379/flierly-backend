462c6d276192084462a6befdfd1f4166ba65b37b
charan379 <37008444+charan379@users.noreply.github.com>
2024-02-18 11:51:56 +0530

    added new method to modify subtypes of accountType

--- src/main/java/com/ctytech/flierly/account/service/AccountTypeService.java
+++ src/main/java/com/ctytech/flierly/account/service/AccountTypeService.java
@@ -1,9 +1,11 @@
 package com.ctytech.flierly.account.service;
 
+import com.ctytech.flierly.account.dto.AccountSubtypeDTO;
 import com.ctytech.flierly.account.dto.AccountTypeDTO;
 import com.ctytech.flierly.account.exception.AccountServiceException;
 
 import java.util.List;
+import java.util.Set;
 
 public interface AccountTypeService {
 
@@ -14,6 +16,8 @@ public interface AccountTypeService {
     List<AccountTypeDTO> fetchAll();
 
     AccountTypeDTO modify(Long id, AccountTypeDTO update) throws AccountServiceException;
+
+    AccountTypeDTO modifySubtypes(Long id, Set<AccountSubtypeDTO> subtypes) throws AccountServiceException;
 
     Boolean existsByName(String name);
 }
--- src/main/java/com/ctytech/flierly/account/service/AccountTypeServiceImpl.java
+++ src/main/java/com/ctytech/flierly/account/service/AccountTypeServiceImpl.java
@@ -1,14 +1,17 @@
 package com.ctytech.flierly.account.service;
 
+import com.ctytech.flierly.account.dto.AccountSubtypeDTO;
 import com.ctytech.flierly.account.dto.AccountTypeDTO;
 import com.ctytech.flierly.account.entity.AccountType;
 import com.ctytech.flierly.account.exception.AccountServiceException;
+import com.ctytech.flierly.account.mapper.AccountSubtypeMapper;
 import com.ctytech.flierly.account.mapper.AccountTypeMapper;
 import com.ctytech.flierly.account.repository.AccountTypeRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
+import java.util.Set;
 
 @Service(value = "accountTypeService")
 public class AccountTypeServiceImpl implements AccountTypeService {
@@ -16,6 +19,10 @@ public class AccountTypeServiceImpl implements AccountTypeService {
     private AccountTypeRepository accountTypeRepository;
     @Autowired
     private AccountTypeMapper accountTypeMapper;
+    @Autowired
+    private AccountSubtypeService accountSubtypeService;
+    @Autowired
+    private AccountSubtypeMapper accountSubtypeMapper;
 
     @Override
     public AccountTypeDTO save(AccountTypeDTO accountTypeDTO) throws AccountServiceException {
@@ -48,6 +55,24 @@ public class AccountTypeServiceImpl implements AccountTypeService {
         // Do update
         accountType.setName(update.getName());
         return accountTypeMapper.toDTO(accountTypeRepository.save(accountType));
+    }
+
+    @Override
+    public AccountTypeDTO modifySubtypes(Long id, Set<AccountSubtypeDTO> subtypes) throws AccountServiceException {
+        AccountType accountType = accountTypeRepository.findById(id).orElseThrow(() -> new AccountServiceException("AccountType.NOT_FOUND"));
+        // Check if provided subtypes set is not null and not empty an set
+        if (subtypes != null && !subtypes.isEmpty()) {
+            // Clear previous set of subtypes
+            accountType.getSubtypes().clear();
+            // For each subtypeDTO check if it exists in db then add it to accountType
+            for (AccountSubtypeDTO subtypeDTO : subtypes) {
+                accountType.getSubtypes().add(accountSubtypeMapper.toEntity(accountSubtypeService.fetch(subtypeDTO.getId())));
+            }
+            // Return new accountType with updated subtypes set
+            return accountTypeMapper.toDTO(accountTypeRepository.save(accountType));
+        } else
+            // if provided subtypes set is a null or empty set just return accountType fetched from db
+            return accountTypeMapper.toDTO(accountType);
     }
 
     @Override
