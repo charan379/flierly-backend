2059e5cfe1cb9e8b9da2aaf330c1bb9d01353a0c
charan379 <37008444+charan379@users.noreply.github.com>
2024-02-19 11:11:02 +0530

    added validation and introduced new fields

--- src/main/java/com/ctytech/flierly/contact/enitity/Contact.java
+++ src/main/java/com/ctytech/flierly/contact/enitity/Contact.java
@@ -1,25 +1,45 @@
 package com.ctytech.flierly.contact.enitity;
 
-import com.ctytech.flierly.organization.entity.Branch;
+import com.ctytech.flierly.address.entity.Address;
 import jakarta.persistence.*;
-import jakarta.validation.constraints.NotNull;
+import jakarta.validation.constraints.Digits;
+import jakarta.validation.constraints.Email;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.Pattern;
 import lombok.EqualsAndHashCode;
 import lombok.Getter;
 import lombok.Setter;
 
 @Entity
 @Table(name = "contacts")
-@Getter @Setter @EqualsAndHashCode
+@SequenceGenerator(name = "contact_id_generator", sequenceName = "contact_id_seq", initialValue = 1000, allocationSize = 1)
+@Getter
+@Setter
+@EqualsAndHashCode
 public class Contact {
 
     @Id
-    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @GeneratedValue(strategy = GenerationType.SEQUENCE, generator = "contact_id_generator")
     private Long id;
 
-    @ManyToOne(cascade = CascadeType.DETACH, fetch = FetchType.EAGER)
-    @JoinColumn(name = "branchId", referencedColumnName = "id", foreignKey = @ForeignKey(name = "contact_branch_fkey"), updatable = false)
-    private Branch branch;
-
-    @NotNull
+    @NotBlank(message = "{contact.name.absent}")
     private String name;
+
+    @Digits(integer = 13, fraction = 0, message = "{phone.invalid}")
+    @NotBlank(message = "{contact.phone.absent}")
+    @Column(length = 13)
+    private String phone;
+
+    @Digits(integer = 13, fraction = 0, message = "{phone.invalid}")
+    @Column(length = 13)
+    private String alternatePhone;
+
+    @Email(message = "{email.invalid}")
+    @Pattern(regexp = "^[a-z0-9._%+-]+@[a-z0-9.-]+\\.[a-z]{2,}$", message = "{email.invalid}")
+    @NotBlank(message = "{contact.email.absent}")
+    private String email;
+
+    @ManyToOne(cascade = CascadeType.DETACH, fetch = FetchType.EAGER)
+    @JoinColumn(name = "addressId", referencedColumnName = "id", foreignKey = @ForeignKey(name = "contact_address_fkey"))
+    private Address address;
 }
