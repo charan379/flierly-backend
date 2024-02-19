d01a72819ce6e3a5a20202224daaba7c14325d41
charan379 <37008444+charan379@users.noreply.github.com>
2024-02-19 14:19:58 +0530

    added ContactServiceException

---
+++ src/main/java/com/ctytech/flierly/contact/exception/ContactServiceException.java
@@ -0,0 +1,9 @@
+package com.ctytech.flierly.contact.exception;
+
+import com.ctytech.flierly.FlierlyException;
+
+public class ContactServiceException extends FlierlyException {
+    public ContactServiceException(String message) {
+        super(message);
+    }
+}
