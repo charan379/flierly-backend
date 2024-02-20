b1e773ad2c2c18c3fde8caeb9635a1d2394cc75e
charan379 <37008444+charan379@users.noreply.github.com>
2024-02-20 17:28:14 +0530

    AccountMapper under construction

---
+++ src/main/java/com/ctytech/flierly/account/mapper/AccountMapper.java
@@ -0,0 +1,61 @@
+package com.ctytech.flierly.account.mapper;
+
+import com.ctytech.flierly.account.dto.AccountDTO;
+import com.ctytech.flierly.account.entity.Account;
+import com.ctytech.flierly.address.mapper.AddressMapper;
+import com.ctytech.flierly.contact.mapper.ContactMapper;
+import com.ctytech.flierly.organization.mapper.BranchMapper;
+import com.ctytech.flierly.taxation.mapper.TaxIdentityMapper;
+import lombok.Getter;
+import lombok.Setter;
+import org.modelmapper.ModelMapper;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Component;
+
+@Component
+public class AccountMapper {
+
+    @Autowired
+    private ModelMapper modelMapper;
+    @Getter
+    @Autowired
+    private BranchMapper branchMapper;
+    @Getter
+    @Autowired
+    private AccountTypeMapper accountTypeMapper;
+    @Getter
+    @Autowired
+    private AccountSubtypeMapper accountSubtypeMapper;
+    @Getter
+    @Autowired
+    private TaxIdentityMapper taxIdentityMapper;
+    @Getter
+    @Autowired
+    private ContactMapper contactMapper;
+    @Getter
+    @Setter
+    private AddressMapper addressMapper;
+
+
+    private AccountDTO toDTO(Account account) {
+        if (account == null) return null;
+        AccountDTO dto = new AccountDTO();
+        dto.setId(account.getId());
+        dto.setIsVip(account.getIsVip());
+        dto.setIsKey(account.getIsKey());
+        dto.setName(account.getName());
+        dto.setRegisteredPhone(account.getRegisteredPhone());
+        dto.setAlternatePhone(account.getAlternatePhone());
+        dto.setEmail(account.getEmail());
+        dto.setBranch(branchMapper.toDTO(account.getBranch(), true));
+        dto.setAccountType(accountTypeMapper.toDTO(account.getAccountType()));
+        dto.setAccountSubtype(accountSubtypeMapper.toDTO(account.getAccountSubtype()));
+        dto.setTaxIdentity(taxIdentityMapper.toDTO(account.getTaxIdentity()));
+//        dto.setContacts();
+        return dto;
+    }
+
+    public Account toEntity(AccountDTO accountDTO) {
+        return modelMapper.map(accountDTO, Account.class);
+    }
+}
