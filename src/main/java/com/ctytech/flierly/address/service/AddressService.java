e5dff17ff2a26c71030d40ae029d21fb62cdb42b
charan379 <37008444+charan379@users.noreply.github.com>
2024-02-25 21:55:50 +0530

    added new method to fetch set of addresses by set of ids

--- src/main/java/com/ctytech/flierly/address/service/AddressService.java
+++ src/main/java/com/ctytech/flierly/address/service/AddressService.java
@@ -3,10 +3,14 @@ package com.ctytech.flierly.address.service;
 import com.ctytech.flierly.address.dto.AddressDTO;
 import com.ctytech.flierly.address.exception.AddressServiceException;
 
+import java.util.Set;
+
 public interface AddressService {
     AddressDTO save(AddressDTO addressDTO) throws AddressServiceException;
 
     AddressDTO fetch(Long id) throws AddressServiceException;
+
+    Set<AddressDTO> fetchAllByIds(Set<Long> ids);
 
     AddressDTO modify(Long id, AddressDTO update) throws AddressServiceException;
 }
